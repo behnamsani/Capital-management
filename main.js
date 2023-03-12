050c7fda6cb1222b59421eda7d43ddd3705a5122
Behnam Sani Ghalhar <sani.behnam14564@gmail.com>
2023-03-12 10:14:53 +0330

    bug fix V2.01

--- main.js
+++ main.js
@@ -20,42 +20,54 @@ btn.addEventListener("click", () => {
   console.log(rC.value);
   if (allC.value == "" || rC.value == "" || lC.value == "" || rpC.value == "") {
     capital.innerHTML = "ورودی ها نمی توانند خالی باشند";
-  } else if (validationNumber(allC.value) === false || validationNumber(lC.value) === false || validationNumber(rpC.value) === false || validationNumber(rC.value) === false) {
+  } else if (
+    validationNumber(allC.value) === false ||
+    validationNumber(lC.value) === false ||
+    validationNumber(rpC.value) === false ||
+    validationNumber(rC.value) === false
+  ) {
     capital.innerHTML = "لطفا فقط عدد وارد نمایید";
   } else {
     e = e.toFixed(2);
     e = parseInt(e);
 
-    capital.innerHTML = `$ ${e} `;
-    const txt = document.createElement("p");
-    txt.innerHTML = "معادل";
-    capital.appendChild(txt);
-    const riskPer = document.createElement("p");
-    riskPer.id = "riskPer";
-    riskPer.innerHTML = `% ${Math.round(
-      (100 * e) / kolesarmaye
-    )} درصد از کل سرمایه را وارد پوزیشن کنید`;
-    capital.appendChild(riskPer);
+    if (e >= kolesarmaye) {
+      capital.innerHTML = `کل سرمایه را می توانید وارد پوزیشن کنید `;
+    } else {
+      capital.innerHTML = `$ ${e} `;
+      const txt = document.createElement("p");
+      txt.innerHTML = "معادل";
+      capital.appendChild(txt);
 
-    const lot = document.createElement("p");
-    lot.id = "lot";
-    lot.innerHTML = `LOT ${ e/100000 }  سایز پوزیشن به لات استاندارد در فارکس `;
-    capital.appendChild(lot);
+      const riskPer = document.createElement("p");
+      riskPer.id = "riskPer";
+      riskPer.innerHTML = `% ${Math.round(
+        (100 * e) / kolesarmaye
+      )} درصد از کل سرمایه را وارد پوزیشن کنید`;
+      capital.appendChild(riskPer);
+
+      const lot = document.createElement("p");
+      lot.id = "lot";
+      lot.innerHTML = `LOT ${
+        e / 100000
+      }  سایز پوزیشن به لات استاندارد در فارکس `;
+      capital.appendChild(lot);
+    }
   }
 });
 
 btnClear.addEventListener("click", () => {
   console.log("btnClear");
-  allC.innerHTML="";
-  allC.value="";
-  lC.innerHTML="";
-  lC.value="";
-  rC.innerHTML="";
-  rC.value="";
-  rpC.innerHTML="";
-  rpC.value="";
+  allC.innerHTML = "";
+  allC.value = "";
+  lC.innerHTML = "";
+  lC.value = "";
+  rC.innerHTML = "";
+  rC.value = "";
+  rpC.innerHTML = "";
+  rpC.value = "";
 
-  result.style.display="none";
+  result.style.display = "none";
   // location.reload();
 });
 
